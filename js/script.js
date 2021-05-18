3278f74b7d26f998e07e7d63799d584a6c5041e6
Zhongqing Shi <zhongqing@createment.team>
2021-05-18 22:47:04 +0200

    responsive Design fertig

--- js/script.js
+++ js/script.js
@@ -16,3 +16,34 @@ function animation(){
     lebenslauf.innerText="SDFSDFSDFSDfs";
     fadeout();
 }
+
+$(document).ready(function(){
+    $(".menu-btn").click(function(){
+        $(".menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+
+    $(".carousel").owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:flase
+            },
+            600:{
+                items:2,
+                nav:flase
+            },
+            1000:{
+                items:3,
+                nav:flase
+            }
+        }
+    });
+})
+    
+
+
