d60d5b1961b768f8cf0a8ef8c49796cd5ea19688
Zhongqing Shi <zhongqing@createment.team>
2021-05-18 10:12:20 +0200

    Sturktur von allen Seiten aufgebaut

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ let lebenslauf=document.getElementById("lebenslauf");
 lebenslauf.addEventListener("animationend", end);
 
 function fadeout(){
-    lebenslauf= document.getElementById("lebenslauf");
+    //lebenslauf= document.getElementById("lebenslauf");
     lebenslauf.style.animation="fade 1s";
 }
 
 
 function end(){
-    lebenslauf= document.getElementById("lebenslauf");
+    //lebenslauf= document.getElementById("lebenslauf");
     lebenslauf.style.animation="";
 }
 
 function animation(){
-    document.getElementById("lebenslauf").innerText="SDFSDFSDFSDfs";
+    lebenslauf.innerText="SDFSDFSDFSDfs";
     fadeout();
 }
